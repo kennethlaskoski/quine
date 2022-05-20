9ee45ee7d85cdb700380e125ffcc7eaa5add65dc
Kenneth Laskoski <kennethlaskoski@gmail.com>
2022-05-20 23:24:15 +0000

    Deleted lowercase folders

--- c++/main.cpp
+++
@@ -1,2 +0,0 @@
-#include<cstdio>
-int main(){char n[]="#include<cstdio>%cint main(){char n[]=%c%s%c;printf(n,10,34,n,34,10);}%c";printf(n,10,34,n,34,10);}
--- swift/main.swift
+++
@@ -1,2 +0,0 @@
-let x = ["let x = ", "\nprint(x[0], x, x[1], separator: String())"]
-print(x[0], x, x[1], separator: String())
